8ff4af2b90aa781324a553d7cf8b3833e2c945e9
BharatRajSingal <126271100+GeekWolf007@users.noreply.github.com>
2023-07-27 02:31:10 +0530

    undo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 import LandingPage from "./Components/LandingPage";
 import Form from "./Form/Form";
 
 function App() {
   return (
-    <BrowserRouter basename="/rocktaves">
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/Form" element={<Form />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/Form" element={<Form />} />
+    </Routes>
   );
 }
 
