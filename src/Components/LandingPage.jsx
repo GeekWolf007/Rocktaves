f81f1acb5923fe40783f34e0daa16b397686ca80
BharatRajSingal <126271100+GeekWolf007@users.noreply.github.com>
2023-07-27 13:47:01 +0530

    custom cursor

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useEffect , useState} from "react";
 import "./LandingPage.css"
 import Heading from "./Heading";
 import Register from "./Register";
@@ -8,9 +8,51 @@ import PastWinners from "./PastWinners";
 import AboutUs from "./AboutUs";
 import Timeline from "./Timeline";
 
-const LandingPage = ({goToForm}) =>{
+const LandingPage = ({}) =>{
+  
+  useEffect(() => {
+    const cursor = document.getElementById("cursor");
+    const cursorCircle = document.getElementById("cursorFollower");
+    console.log(cursorCircle)
+
+
+    if (window.innerWidth >= 1000) {
+      window.onpointermove = (event) => {
+        const { clientX, clientY } = event;
+
+        cursor.style.display = "block";
+        cursorCircle.style.display = "block";
+        cursor.style.opacity = "1";
+        cursorCircle.style.opacity = "1";
+        cursor.style.left = `${clientX}px`;
+        cursor.style.top = `${clientY}px`;
+
+        let _clientX = clientX - 12;
+        let _clientY = clientY - 12;
+
+        cursorCircle.animate(
+          {
+            left: `${_clientX}px`,
+            top: `${_clientY}px`,
+          },
+          { duration: 600, fill: "forwards" }
+        );
+      };
+    }
+    return () => {
+      window.onpointermove = null;
+    };
+  }, []);
+
+
+
+
+
+
   return (
     <div className="landingPageWrapper">
+      <div className="cursor" id="cursor"></div>
+      <div className="cursorFollower" id="cursorFollower"></div>
       <section className="headingWrapper">
         <div className="fixedBG"></div>
         <Heading />
