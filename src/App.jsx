0d47f369d8daf01a08664c15d79dcdb575b93868
BharatRajSingal <126271100+GeekWolf007@users.noreply.github.com>
2023-07-27 07:28:06 +0530

    undo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import { createBrowserHistory } from "history";
 import LandingPage from "./Components/LandingPage";
 import Form from "./Form/Form";
 
 function App() {
-
-  const history = createBrowserHistory({ basename: '/rocktaves' });
-
   return (
-    <Routes history={history}>
+    <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/Form" element={<Form />} />
     </Routes>
