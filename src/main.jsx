3d0d2c0a0ad8a1d3de1b98b359541d5c9f587853
BharatRajSingal <126271100+GeekWolf007@users.noreply.github.com>
2023-07-27 21:45:46 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/rocktaves'>
     <Routes>
       <Route path="*" element={<App />} />
     </Routes>
