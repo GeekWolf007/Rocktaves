783e57741f12f42a440f177fa9874718aea2f046
BharatRajSingal <126271100+GeekWolf007@users.noreply.github.com>
2023-07-27 16:09:01 +0530

    basepath

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Routes } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import LandingPage from "./Components/LandingPage";
 import Form from "./Form/Form";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/Form" element={<Form />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Form" element={<Form />} />
+      </Routes>
   );
 }
 
